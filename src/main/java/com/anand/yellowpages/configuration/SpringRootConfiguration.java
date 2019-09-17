676f312fcaf420f854e72df03c6a7a36a263d131
A Anand <aanan@MSI>
2019-09-17 22:18:03 +0530

    Initial Project Commit

---
+++ src/main/java/com/anand/yellowpages/configuration/SpringRootConfiguration.java
@@ -0,0 +1,15 @@
+package com.anand.yellowpages.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * @author A Anand
+ *
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.anand.yellowpages"})
+public class SpringRootConfiguration {
+
+}
